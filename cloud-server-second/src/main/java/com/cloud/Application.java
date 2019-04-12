7b66f143f263d604e23c20de6716b4c7325c768f
children2018 <Evj86@163.com>
2019-04-13 00:26:12 +0800

    fdsafdsafds本地

--- cloud-server-second/src/main/java/com/cloud/Application.java
+++ cloud-server-second/src/main/java/com/cloud/Application.java
@@ -32,7 +32,7 @@ public class Application {
 	}
 
 	public static void main(String[] fdsafdsfdsaf) {
-		System.out.println("11111111");
+		System.out.println("11111111sssssssssss");
 		SpringApplication.run(Application.class, fdsafdsfdsaf);
 		System.out.println("gameddd");
 	}
