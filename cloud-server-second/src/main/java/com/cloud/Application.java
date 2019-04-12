d1e08459e8fe357cebf4ff8bbe205cacc0345eac
children2018 <Evj86@163.com>
2019-04-12 21:49:57 +0800

    又来了

--- cloud-server-second/src/main/java/com/cloud/Application.java
+++ cloud-server-second/src/main/java/com/cloud/Application.java
@@ -21,7 +21,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("yaya///");
-		System.out.println("fdskf");
 		SpringApplication.run(Application.class, args);
 		System.out.println("game over...");
 	}
