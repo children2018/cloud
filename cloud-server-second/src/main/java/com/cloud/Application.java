6b6ab5ff2cc65c2c1693befd5b69436c68105e1d
children2018 <Evj86@163.com>
2019-04-12 23:35:55 +0800

    GG

--- cloud-server-second/src/main/java/com/cloud/Application.java
+++ cloud-server-second/src/main/java/com/cloud/Application.java
@@ -23,9 +23,9 @@ public class Application {
 		return null;
 	}
 
-	public static void main(String[] argssssssssssssssSSS11111111111) {
+	public static void main(String[] A) {
 		System.out.println("y");
-		SpringApplication.run(Application.class, argssssssssssssssSSS11111111111);
+		SpringApplication.run(Application.class, A);
 		System.out.println("game");
 	}
 }
