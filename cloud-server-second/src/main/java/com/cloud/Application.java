84582dc362becde3964b9222787bbfcb51a54f37
children2018 <Evj86@163.com>
2019-04-13 01:42:25 +0800

    d

--- cloud-server-second/src/main/java/com/cloud/Application.java
+++ cloud-server-second/src/main/java/com/cloud/Application.java
@@ -32,7 +32,7 @@ public class Application {
 	}
 
 	public static void main(String[] fdsafdsfdsaf) {
-		System.out.println("子子孙孙bbbbbbbb子子孙孙aaa");
+		System.out.println("子子孙孙bbbbbbbb子子孙孙aaab");
 		SpringApplication.run(Application.class, fdsafdsfdsaf);
 		System.out.println("gameddd");
 	}
