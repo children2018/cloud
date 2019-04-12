9fcae6a64a4086b5c9994a406fd0f6116cee6350
children2018 <Evj86@163.com>
2019-04-13 02:17:32 +0800

    待合并代码

--- cloud-server-second/src/main/java/com/cloud/Application.java
+++ cloud-server-second/src/main/java/com/cloud/Application.java
@@ -32,7 +32,7 @@ public class Application {
 	}
 
 	public static void main(String[] fdsafdsfdsaf) {
-		System.out.println("子");
+		System.out.println("子魂牵梦萦本地标有村");
 		SpringApplication.run(Application.class, fdsafdsfdsaf);
 		System.out.println("gameddd");
 	}
