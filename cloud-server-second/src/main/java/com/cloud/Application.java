58aafb50d225a970fbe29e824b471182f31cf037
children2018 <Evj86@163.com>
2019-04-14 03:49:04 +0800

    1232321

--- cloud-server-second/src/main/java/com/cloud/Application.java
+++ cloud-server-second/src/main/java/com/cloud/Application.java
@@ -32,7 +32,7 @@ public class Application {
 	}
 
 	public static void main(String[] fdsafdsfdsaf) {
-		System.out.println("ss");
+		System.out.println("ssdsafdsf");
 		SpringApplication.run(Application.class, fdsafdsfdsaf);
 		System.out.println("gameddd3333333334");
 	}
