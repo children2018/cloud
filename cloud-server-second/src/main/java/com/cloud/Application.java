24e083db66904e6fdf0e5927d756efecd5f3e4bf
children2018 <Evj86@163.com>
2019-04-14 04:12:40 +0800

    fdsf

--- cloud-server-second/src/main/java/com/cloud/Application.java
+++ cloud-server-second/src/main/java/com/cloud/Application.java
@@ -32,7 +32,7 @@ public class Application {
 	}
 
 	public static void main(String[] fdsafdsfdsaf) {
-		System.out.println("ssdsafdsff34");
+		System.out.println("ssdsafdsff345");
 		SpringApplication.run(Application.class, fdsafdsfdsaf);
 		System.out.println("gameddd3333333334");
 	}
